b5b2c711459b5dd6833a97a3ad8964f06f2dc26a
DavidReyes0510 <david.reyes253@std.uni.edu.ni>
2025-05-23 23:00:29 -0600

    subiendo Transacciones_Service

---
+++ BancoSimple2M5/Services/Transferencia_Service.cs
@@ -0,0 +1,62 @@
+﻿using BancoSimple2M5.Data;
+using BancoSimple2M5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancoSimple2M5.Services
+{
+    public class Transferencia_Service
+    {
+        //Declaración de la dependencia con la base de datos
+        private readonly BancoSimple2M5Context _context;
+
+        public Transferencia_Service(BancoSimple2M5Context context)
+        {
+            _context = context;
+        }
+
+        public void RealizarTransferencia(int origenID, int destinoID, decimal monto, string descripción)
+        {
+            //Transacciones 
+            //Niveles de aislamiento
+            using var transacción = _context.Database.BeginTransaction(System.Data.IsolationLevel.Serializable);
+            {
+                try
+                {
+                    var cuentaOrigen = _context.Cuenta.FirstOrDefault(c => c.CuentaID == origenID);
+                    var cuentaDestino = _context.Cuenta.FirstOrDefault(c => c.CuentaID == destinoID);
+                    if (cuentaOrigen.Saldo < monto)
+                    {
+                        throw new Exception("Saldo insuficiente");
+                    }
+                    else
+                    {
+                        cuentaOrigen.Saldo -= monto;
+                        cuentaDestino.Saldo += monto;
+
+                        //Registrar la transacción
+
+                        _context.Transaccion.Add(new Transaccion
+                        {
+                            Monto = monto,
+                            Fecha = DateTime.Now,
+                            Descripción = descripción,
+                            CuentaOrigenId = origenID,
+                            CuentaDestinoId = destinoID
+                        });
+                        _context.SaveChanges();
+                        //Transacción Completada
+                        transacción.Commit();
+                        MessageBox.Show("Transferencia realizada con exito", "Exto en operación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Reversión de transacción
+                    transacción.Rollback();
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+    }
+}
--- BancoSimple2M5/obj/Debug/net8.0-windows/BancoSimple2M5.AssemblyInfo.cs
+++ BancoSimple2M5/obj/Debug/net8.0-windows/BancoSimple2M5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BancoSimple2M5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03740bf45eb63c1a4512d5462f4957e172834652")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba642f7b28421119d026fd8e48674ea02b872cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BancoSimple2M5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BancoSimple2M5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
